16ef26c00d30f7c187f9bc85cc52424eab4825f4
Khurram Virani <kvirani@gmail.com>
2013-07-02 12:06:17 -0400

    Initial commit, made after class finished.

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'carmen'
+
+# INDEX
+get '/countries' do
+  @countries = Carmen::Country.all
+  erb :index
+end
+
+# We could do this, but of course it's silly. 
+#  See public folder for static/public assets
+# get 'style.css' do
+#   "h1 {color: red}"
+# end
+
+# SHOW
+get '/countries/:code' do
+  @country = Carmen::Country.coded(params[:code])
+  erb :show
+end 
+
